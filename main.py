29d0ad8132f88a6b6838d83634975cbe2ccbb67c
NdukuGrace <grace.nguli@strathmore.edu>
2024-05-22 14:48:00 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import tasks
+from time import time
+
+INPUT_FILE_PATH = 'input_data_files/'
+OUTPUT_FILE_PATH = 'output_data_files/'
+
+if __name__ == '__main__':
+    print(time())
